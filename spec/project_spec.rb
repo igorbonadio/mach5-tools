97a49bf4ed6bc8b923723b4ef294ccab1968ebc2
Igor Bonadio <igorbonadio@gmail.com>
2014-01-29 17:10:58 -0200

    Updates example

--- spec/project_spec.rb
+++ spec/project_spec.rb
@@ -11,22 +11,20 @@ module Mach5Tools
     end
 
     it "should have a list of commands to execute before benchmarks" do
-      Kernel.should_receive(:system).with("cd build")
-      Kernel.should_receive(:system).with("cmake ..")
-      Kernel.should_receive(:system).with("make")
+      Kernel.should_receive(:system).with("cd build && cmake ..")
+      Kernel.should_receive(:system).with("cd build && make")
       @project.before
     end
 
     it "should have a list of commands to execute benchmarks" do
       output = double("Output")
       output.stub(:readlines).and_return(["output"])
-      IO.should_receive(:popen).with("./benchmark/benchmark test1 test2").and_return(output)
+      IO.should_receive(:popen).with("./build/benchmark/benchmark test1 test2").and_return(output)
       @project.run(["test1", "test2"])
     end
 
     it "should have a list of commands to execute after benchmarks" do
-      Kernel.should_receive(:system).with("make clean")
-      Kernel.should_receive(:system).with("cd ..")
+      Kernel.should_receive(:system).with("cd build && make clean")
       @project.after
     end
 
@@ -36,24 +34,20 @@ module Mach5Tools
 
     it "should run all benchmarks" do
       Kernel.should_receive(:system).with("git checkout ab7c4351a13b29ea4c21e3662f9f567ff19a854d")
-      Kernel.should_receive(:system).with("cd build")
-      Kernel.should_receive(:system).with("cmake ..")
-      Kernel.should_receive(:system).with("make")
+      Kernel.should_receive(:system).with("cd build && cmake ..")
+      Kernel.should_receive(:system).with("cd build && make")
       output = double("Output")
       output.stub(:readlines).and_return(["output"])
-      IO.should_receive(:popen).with("./benchmark/benchmark AHiddenMarkovModelEvaluatesSequences AHiddenMarkovModelFindsTheBestPath AHiddenMarkovModelCalculatesProbabilityOfObservationsUsingForward AHiddenMarkovModelCalculatesProbabilityOfObservationsUsingBackward AHiddenMarkovModelDecodesASequenceOfObservationsUsingThePosteriorProbability").and_return(output)
-      Kernel.should_receive(:system).with("make clean")
-      Kernel.should_receive(:system).with("cd ..")
+      IO.should_receive(:popen).with("./build/benchmark/benchmark AHiddenMarkovModelEvaluatesSequences AHiddenMarkovModelFindsTheBestPath AHiddenMarkovModelCalculatesProbabilityOfObservationsUsingForward AHiddenMarkovModelCalculatesProbabilityOfObservationsUsingBackward AHiddenMarkovModelDecodesASequenceOfObservationsUsingThePosteriorProbability").and_return(output)
+      Kernel.should_receive(:system).with("cd build && make clean")
 
       Kernel.should_receive(:system).with("git checkout c031c8e9afe1493a81274adbdb61b81bc30ef522")
-      Kernel.should_receive(:system).with("cd build")
-      Kernel.should_receive(:system).with("cmake ..")
-      Kernel.should_receive(:system).with("make")
+      Kernel.should_receive(:system).with("cd build && cmake ..")
+      Kernel.should_receive(:system).with("cd build && make")
       output = double("Output")
       output.stub(:readlines).and_return(["output"])
-      IO.should_receive(:popen).with("./benchmark/benchmark ALinearChainCRFFindsTheBestPath ALinearChainCRFCalculatesProbabilityOfObservationsUsingForward ALinearChainCRFCalculatesProbabilityOfObservationsUsingBackward ALinearChainCRFDecodesASequenceOfObservationsUsingThePosteriorProbability").and_return(output)
-      Kernel.should_receive(:system).with("make clean")
-      Kernel.should_receive(:system).with("cd ..")
+      IO.should_receive(:popen).with("./build/benchmark/benchmark ALinearChainCRFFindsTheBestPath ALinearChainCRFCalculatesProbabilityOfObservationsUsingForward ALinearChainCRFCalculatesProbabilityOfObservationsUsingBackward ALinearChainCRFDecodesASequenceOfObservationsUsingThePosteriorProbability").and_return(output)
+      Kernel.should_receive(:system).with("cd build && make clean")
 
       @project.run_all
     end
