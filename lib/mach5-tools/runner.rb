75bb0c4551f223e1833e2fa78c8989e52f4d9a8d
Igor Bonadio <igorbonadio@gmail.com>
2014-02-07 11:57:11 -0200

    mach5 runs all benchmarks but doesn't analyses them

--- lib/mach5-tools/runner.rb
+++ lib/mach5-tools/runner.rb
@@ -14,7 +14,7 @@ module Mach5
 
     def run(benchmarks)
       @config.run_commands.each do |command|
-        Kernel.system "#{command} #{benchmarks.join(' ')}"
+        Kernel.system "#{command} run #{benchmarks.join(' ')}"
       end
     end
 
