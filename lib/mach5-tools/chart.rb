ac4f941160dbaf6a0522471166a238c2b5a627fe
Igor Bonadio <igorbonadio@gmail.com>
2014-02-17 10:59:53 -0300

    chart class

---
+++ lib/mach5-tools/chart.rb
@@ -0,0 +1,48 @@
+module Mach5
+  class Chart
+    attr_accessor :type
+    attr_accessor :data_type
+    attr_accessor :size
+    attr_accessor :title
+    attr_accessor :x_axis
+    attr_accessor :y_axis
+    attr_accessor :series
+    attr_accessor :folder
+
+    def build
+      {
+        "type" => "line",
+        "dataType" => "runs_total_time",
+        "size" => {
+          "width" => size.split("x").map(&:to_i)[0],
+          "height" => size.split("x").map(&:to_i)[1]
+        },
+        "title" => {
+          "text" => title
+        },
+        "xAxis" => {
+          "title" => {
+            "text" => x_axis
+          }
+        },
+        "yAxis" => {
+          "title" => {
+            "text" => y_axis
+          }
+        },
+        "series" => _series(series)
+      }
+    end
+
+    def _series(series)
+      result = []
+      series.each do |s|
+        result << {
+          "label" => "#{s[0]}.#{s[1]}",
+          "file" =>  File.join(@folder, "#{s[0]}.#{s[1]}.json")
+        }
+      end
+      result
+    end
+  end
+end
---
+++ spec/chart_spec.rb
@@ -0,0 +1,48 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+
+module Mach5
+  describe Chart do
+    before(:each) do
+      @chart = Chart.new
+      @chart.folder = "_benchmark"
+      @chart.type = "line"
+      @chart.data_type = "runs_total_time"
+      @chart.size = "100x200"
+      @chart.title = "Viterbi vs Posterior Decoding"
+      @chart.x_axis = "Sequence Size"
+      @chart.y_axis = "Time (s)"
+      @chart.series = [["edd0982eed0c414631991aa1dea67c811d95373f", "DishonestCasinoHMM.Viterbi"], ["edd0982eed0c414631991aa1dea67c811d95373f", "DishonestCasinoHMM.PosteriorDecoding"]]
+    end
+
+    it "should return a hash" do
+      @chart.build.should be == {
+        "type" => "line",
+        "dataType" => "runs_total_time",
+        "size" => {
+          "width" => 100,
+          "height" => 200
+        },
+        "title" => {
+          "text" => "Viterbi vs Posterior Decoding"
+        },
+        "xAxis" => {
+          "title" => {
+            "text" => "Sequence Size"
+          }
+        },
+        "yAxis" => {
+          "title" => {
+            "text" => "Time (s)"
+          }
+        },
+        "series" => [{
+          "label" => "edd0982eed0c414631991aa1dea67c811d95373f.DishonestCasinoHMM.Viterbi",
+          "file" =>  "_benchmark/edd0982eed0c414631991aa1dea67c811d95373f.DishonestCasinoHMM.Viterbi.json"
+        },{
+          "label" => "edd0982eed0c414631991aa1dea67c811d95373f.DishonestCasinoHMM.PosteriorDecoding",
+          "file" =>  "_benchmark/edd0982eed0c414631991aa1dea67c811d95373f.DishonestCasinoHMM.PosteriorDecoding.json"
+        }]
+      }
+    end
+  end
+end
